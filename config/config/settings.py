62dee4c59a00c3aa75df86186b1d3f6976c76836
Shohijahon <autovibes1111@gmail.com>
2026-05-12 13:10:12 +0500

    yangilandi1

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-$iz96=qaxxxej-unp8sdbu5n&8cg$#)=2_+)s1=17qp!(*yn3r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'tmk.pythonanywhere.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
